5a6bea2b38c0aeb5b800302a66d76ce737533d57
Hamed Fathi <hami.fthi@gmail.com>
2024-06-01 21:38:56 +0200

    Add logging config

---
+++ logging_config.py
@@ -0,0 +1,12 @@
+# logging_config.py
+import logging
+
+
+def setup_logging():
+    logging.basicConfig(
+        format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG
+    )
+    logger = logging.getLogger(__name__)
+    return logger
